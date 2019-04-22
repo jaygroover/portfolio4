aff23538eead66ffefa71004c696b8ec09626876
jaygroover1 <jaygroover@gmail.com>
2019-04-21 23:14:17 -0400

    second update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
   //$(".card").hide ();
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyARvIGxvt3fdLueKbI6y0K8OPK5IIVcUss",
+  authDomain: "fir-c8408.firebaseapp.com",
+  databaseURL: "https://fir-c8408.firebaseio.com",
+  projectId: "fir-c8408",
+  storageBucket: "fir-c8408.appspot.com",
+  messagingSenderId: "305536823044"
+};
+firebase.initializeApp(config);
+var dataBase = firebase.database()
+$("#submit").on("click", function () {
 
+    var reviews = {
+        videoName: "test",
+        guestReview: $("#textarea").val().trim(),
+        timeStamp: new Date().toString()
+    };
+
+    //console.log(reviews);
+
+    dataBase.ref().push(reviews)
+   
+})
 
   
   
